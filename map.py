af5f1362061857ee08abf2185f2f5d5e3461cbac
Cameron Bolitho <C1806809@cf.ac.uk>
2018-10-24 19:46:21 +0000

    deleted a leia

--- map.py
+++ map.py
@@ -111,7 +111,7 @@ location_leiascell = {
     """Leia's cell, you can see her through the window. She looks back at you in relief and rushes
     to the door.""",
     
-    "items": [item_leia],
+    "items": [],
 
     "exits": {"west": "Death Star"}
 }
